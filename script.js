ae3cd905b8d051406b65304fddcfa9adee3dcbd1
pajriahagusti34-beep <pajriahagusti34@gmail.com>
2026-07-23 16:18:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const tombol = document.getElementById("openGift");
 
 const welcome = document.querySelector(".welcome");
-
 const envelopePage = document.querySelector(".envelope-page");
+
+const envelope = document.querySelector(".envelope");
+const gallery = document.querySelector(".gallery");
+
 
 tombol.addEventListener("click", function(){
 
     welcome.style.display = "none";
-
     envelopePage.style.display = "block";
+
+});
+
+
+envelope.addEventListener("click", function(){
+
+    gallery.style.display = "block";
 
 });
